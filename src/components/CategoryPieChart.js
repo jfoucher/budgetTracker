7077a1732256fe99a10fc4924832319b7f8b5a15
jfoucher <jfoucher@6px.eu>
2017-01-14 16:35:33 +0100

    Only update pie chart when transactions changed. Remove call to getCategories, replaced by passing the colors in the data

--- src/components/CategoryPieChart.js
+++ src/components/CategoryPieChart.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import {PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 
 class CategoryPieChart extends Component {
+    
     render(){
-        const slices = this.props.categories.map((c) => {
+        const slices = this.props.data.map((c) => {
             return <Cell key={c.name} isAnimationActive={false} dataKey={c.name} stackId="a" fill={c.color} />
             //return <Bar key={c.name} isAnimationActive={false} dataKey={c.name} stackId="a" fill={c.color} />
         });
@@ -12,6 +13,7 @@ class CategoryPieChart extends Component {
             <ResponsiveContainer width="100%" height={300}>
                 <PieChart height={500} onMouseEnter={this.onPieEnter}>
                     <Pie
+
                         data={this.props.data}
                         outerRadius={120}
                         fill="#8884d8"
--- src/containers/CategoryPieChartContainer.js
+++ src/containers/CategoryPieChartContainer.js
@@ -3,6 +3,7 @@ import CategoryPieChart from '../components/CategoryPieChart'
 import moment from 'moment'
 
 const getData = (transactions, filter) => {
+
     //Return transaction that correspond to the month given in the filter
     const thisMonth = transactions.filter((transaction) => {
         const date = moment(transaction.date);
@@ -15,9 +16,9 @@ const getData = (transactions, filter) => {
     for (var i = thisMonth.length; i--;) {
         var t = thisMonth[i];
         if(cats[t.category.name] === undefined) {
-            cats[t.category.name] = t.amount;
+            cats[t.category.name] = {amount: t.amount, color: t.category.color};
         } else {
-            cats[t.category.name] += t.amount;
+            cats[t.category.name].amount += t.amount;
         }
         thisMonth.splice(i, 1);
     }
@@ -26,7 +27,7 @@ const getData = (transactions, filter) => {
 
     for(var c in cats) {
         if(cats.hasOwnProperty(c)) {
-            data.push({name: c, value: cats[c]});
+            data.push({name: c, value: cats[c].amount, color: cats[c].color});
         }
     }
 
@@ -36,24 +37,12 @@ const getData = (transactions, filter) => {
 
 }
 
-const getCategories = (transactions, filter) => {
-    //console.log(transactions);
-    return transactions.filter((transaction) => {
-        const date = moment(transaction.date);
-        const str = date.format('YYYYMM');
-        return str === filter
-    }).map((t) => {
-        return t.category;
-    }).filter((obj, pos, arr) => {
-        return arr.map(mapObj => mapObj['name']).indexOf(obj['name']) === pos;
-    });
-}
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps, a, b, c) => {
     return {
         data: getData(state.transactions, state.visibilityFilter),
-        categories: getCategories(state.transactions, state.visibilityFilter),
-        month: state.visibilityFilter
+        month: state.visibilityFilter,
+        sample: !Boolean(state.transactions.length)
     }
 }
 
@@ -65,7 +54,16 @@ const mapDispatchToProps = (dispatch) => {
 
 const CategoryPieChartContainer = connect(
     mapStateToProps,
-    mapDispatchToProps
+    mapDispatchToProps,
+    (stateProps, dispatchProps, ownProps) => {
+        return Object.assign({}, ownProps, stateProps, dispatchProps)
+    },
+    {
+        pure: true,
+        areStatesEqual: (prev, next) => {
+            return prev.transactions === next.transactions;
+        }
+    }
 )(CategoryPieChart);
 
 export default CategoryPieChartContainer
