d6131442289c51f56826a32503516f768372862b
jfoucher <jfoucher@6px.eu>
2017-01-14 23:15:18 +0100

    Make title text thinner

--- src/components/TitleH2.js
+++ src/components/TitleH2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-grid-system'
+import muiThemeable from 'material-ui/styles/muiThemeable';
 
 class TitleH2 extends Component {
 
@@ -7,10 +8,11 @@ class TitleH2 extends Component {
         return (
             <Row>
                 <Col sm={12}>
-                    <h2>Transactions for {this.props.month}</h2>
+                    <h2 style={{fontWeight: 300, color:this.props.muiTheme.palette.textColor}}>Transactions for {this.props.month}</h2>
                 </Col>
             </Row>
         );
     }
 }
-export default TitleH2;
+
+export default muiThemeable()(TitleH2);
