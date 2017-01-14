5d32cf976381f8e2d8f765f7334f1333b276aac0
jfoucher <jfoucher@6px.eu>
2017-01-14 23:16:46 +0100

    Use slightly darker grey for accent color

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {fade} from 'material-ui/utils/colorManipulator';
 import { teal500, teal700,
     orange500,
-    grey100, grey300, grey400, grey500,
+    grey100, grey300, grey400, grey600,
     white, darkBlack, fullBlack,} from 'material-ui/styles/colors';
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -18,13 +18,15 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 
 const muiTheme = getMuiTheme({
+    fontFamily: 'Roboto, sans-serif',
+    fontWeight: 300,
     palette: {
         primary1Color: teal500,
         primary2Color: teal700,
         primary3Color: grey400,
         accent1Color: orange500,
         accent2Color: grey100,
-        accent3Color: grey500,
+        accent3Color: grey600,
         textColor: darkBlack,
         alternateTextColor: white,
         canvasColor: white,
